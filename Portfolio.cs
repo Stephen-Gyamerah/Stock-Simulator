0ef34a45d3085a3df529990e829d6fa69dbb817b
Iam_StephenG <gyamerah284@gmail.com>
2025-09-20 04:20:40 +0100

    Create Portfolio.cs

---
+++ Portfolio.cs
@@ -0,0 +1,23 @@
+// Portfolio.cs 
+
+// { 
+
+// Purpose: Represents a user's investment account. 
+
+ 
+
+// What it should do: 
+
+// Track how much cash the user has  
+
+// {//-- Create a variable called cashAvailable --//} 
+
+// Store how many shares of each stock they own  
+
+// {// -- Create a variable called stockShare --//  
+
+// Help calculate total portfolio value 
+
+// -- Create a variable called portfolioValue --//  
+
+// } 
