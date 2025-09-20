35a797bd4b14998db85439301cdf89569ae20ea1
Iam_StephenG <gyamerah284@gmail.com>
2025-09-20 04:21:23 +0100

    Create Transactions.cs

---
+++ Transactions.cs
@@ -0,0 +1,13 @@
+// Purpose: Records each buy or sell action. 
+
+ 
+
+// What it should do: 
+
+/// Store the ticker, quantity, price, and time of the trade 
+
+// Indicate where it was a "BUY" or "Sell" 
+
+// Help with tracking history or generating reports  
+
+ 
