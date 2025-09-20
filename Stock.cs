dcf8be3fc00bf2043816f369bb4ce7dd22fcdee8
Iam_StephenG <gyamerah284@gmail.com>
2025-09-20 04:19:25 +0100

    Create Stock.cs

---
+++ Stock.cs
@@ -0,0 +1,17 @@
+// -- Stock.cs <<// -- Bonus point learn how to work with APIs and use YFinance APIS to make it easier to get all the data --//>> 
+
+// -- { 
+
+// -- Purpose: Represents a single stock in the market. 
+
+ 
+
+// -- What it should do: 
+
+// -- Store the stock's ticker symbol (e.g, "AAPL") 
+
+// -- Track its current price 
+
+// -- Keep a history of past price for analysis or graphing 
+
+// --- } 
